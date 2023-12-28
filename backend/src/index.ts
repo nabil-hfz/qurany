812682e14ef6f5beef6020a8ebe0ab91dd310211
nabil.alhafez <nabil.alhfz98@gmail.com>
2023-12-28 13:45:06 +0200

    Adds a new api documentation throughout swagger and updates README.md file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,10 +29,10 @@ app.use(timeout(1000 * 60 * 3))
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
-// app.use(bodyParser.text({ limit: '1024mb' }))
-// app.use(bodyParser.raw({ limit: '1024mb' }))
+
 const maxFileSize = 1024 * 1024 * 1024 * 5;
 app.use(bodyParser.json({ limit: maxFileSize }))
+
 app.use(bodyParser.urlencoded({ limit: maxFileSize, extended: true, parameterLimit: maxFileSize }))
 
 interceptors.forEach((interceptor) => {
@@ -44,19 +44,20 @@ CONTROLLERS.forEach((controller) => {
   controller.initialize(httpServer);
 });
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app._router.use('/api-docs', swaggerUi.serve);
-app._router.get('/api-docs', swaggerUi.setup(swaggerSpec));
+// app._router.use('/api-docs', swaggerUi.serve);
+// app._router.get('/api-docs', swaggerUi.setup(swaggerSpec));
 // app.router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // const swaggerSpec = swaggerJSDoc(options);
 
-app.use('/api-docs2', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use('/api-docs2', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hellow world!');
+  res.status(200).send('Hellow world! ');
 });
+
 app.get('/health', (req, res) => {
   res.status(200).send('Health: OK');
 });
@@ -66,8 +67,8 @@ const PORT = process.env.PORT || 8085;
 app.listen(PORT, () => {
   log(`Server listening on port http://localhost:${PORT}...`);
 });
-// https://www.youtube.com/watch?v=CqY2kYJQoK0
 
+// https://www.youtube.com/watch?v=CqY2kYJQoK0
 // https://www.youtube.com/watch?v=2Ti6r34odOw
 // https://www.youtube.com/watch?v=0YTs40kvnW0&list=PLjl2dJMjkDjkBIKd_S9YeBMsT92L8KD4m&index=11
 // https://www.youtube.com/watch?v=S45jZCvd2M8&t=43s
