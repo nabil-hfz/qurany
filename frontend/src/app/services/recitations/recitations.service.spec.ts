0c31a9556f6bcd780a028d09fc0ee74eba17d366
nabil.alhafez <nabil.alhfz98@gmail.com>
2024-01-15 23:15:20 +0200

    Adds recitation service test file

--- frontend/src/app/services/recitations/recitations.service.spec.ts
+++ frontend/src/app/services/recitations/recitations.service.spec.ts
@@ -1,6 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RecitationsService } from './recitations.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { RecitationModel, RecitationTypes } from '../../models/recitation.model';
+import { throwError, of } from 'rxjs';
+import { BaseFilter } from '../../models/filters/base.filter';
 
 describe('RecitationsService', () => {
   let service: RecitationsService;
