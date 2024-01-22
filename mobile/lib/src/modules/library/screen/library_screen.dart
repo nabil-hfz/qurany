7377e576fe5a555cb56dfcb6636a25b7d99014a5
nabil.alhafez <nabil.alhfz98@gmail.com>
2024-01-23 00:04:48 +0200

    # Task - Adds new list library.

--- mobile/lib/src/modules/library/screen/library_screen.dart
+++ mobile/lib/src/modules/library/screen/library_screen.dart
@@ -2,12 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kawtharuna/src/core/bloc/base/states/base_fail_state.dart';
+import 'package:kawtharuna/src/core/constants/app_text_style.dart';
 import 'package:kawtharuna/src/core/di/di.dart';
 import 'package:kawtharuna/src/core/managers/localization/app_translation.dart';
 import 'package:kawtharuna/src/core/managers/theme/app_them_manager.dart';
 import 'package:kawtharuna/src/core/utils/utl_device.dart';
 import 'package:kawtharuna/src/core/widgets/app_bar/salony_app_bar.dart';
 import 'package:kawtharuna/src/core/widgets/error/app_error_widget.dart';
+import 'package:kawtharuna/src/core/widgets/image/app_image_widget.dart';
 import 'package:kawtharuna/src/core/widgets/loader/app_loading_indicator.dart';
 import 'package:kawtharuna/src/modules/library/domain/blocs/library_cubit.dart';
 import 'package:provider/provider.dart';
@@ -59,10 +61,33 @@ class _LibraryScreenState extends State<LibraryScreen> {
                 final fileEntries =
                     (state.getLibraryFileEntries as LibraryFileEntriesSuccess)
                         .fileEntries;
-                return ListView.builder(
+                return GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                  ),
+                  padding: EdgeInsets.all(8.0),
+                  itemCount: fileEntries.length,
                   itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(fileEntries[index].name),
+                    final file = fileEntries[index];
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: AppImageWidget(path: file.thumbnail),
+                        ),
+                        Container(
+                          color: Colors.grey.shade200,
+                          child: Center(
+                            child: Text(
+                              file.name,
+                              style: appTextStyle.semiBold16.copyWith(
+                                color: themeStore.appColors.textColor,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     );
                   },
                 );
