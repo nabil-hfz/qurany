463f40f9a0a510d919c0f1ca943fdcd70995d4ca
nabil.alhafez <nabil.alhfz98@gmail.com>
2024-01-23 23:52:35 +0200

    # Task - Adds totaldownloads section.

--- mobile/lib/src/modules/library/screen/library_screen.dart
+++ mobile/lib/src/modules/library/screen/library_screen.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kawtharuna/src/core/bloc/base/states/base_fail_state.dart';
+import 'package:kawtharuna/src/core/constants/app_icon_size.dart';
 import 'package:kawtharuna/src/core/constants/app_text_style.dart';
 import 'package:kawtharuna/src/core/di/di.dart';
 import 'package:kawtharuna/src/core/managers/localization/app_translation.dart';
@@ -103,48 +104,67 @@ class _LibraryScreenState extends State<LibraryScreen> {
                   itemCount: fileEntries.length,
                   itemBuilder: (context, index) {
                     final file = fileEntries[index];
-                    return Container(
-                      color: Colors.red,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: AppImageWidget(
-                              path: file.thumbnail,
-                              boxFit: BoxFit.cover,
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: AppImageWidget(
+                            path: file.thumbnail,
+                            boxFit: BoxFit.cover,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            HorizontalTextPadding.with2(),
+                            Text(
+                              file.name,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: appTextStyle.semiBold16.copyWith(
+                                color: themeStore.appColors.textColor,
+                              ),
                             ),
-                          ),
-                          Row(
-                            children: [
-                              HorizontalTextPadding.with2(),
-                              Text(
-                                file.name,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: appTextStyle.semiBold16.copyWith(
-                                  color: themeStore.appColors.textColor,
-                                ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            //
+                            Icon(
+                              Icons.visibility_rounded,
+                              color: themeStore.appColors.iconGreyColor,
+                              size: AppIconSize.size_20,
+                            ),
+                            HorizontalTextPadding.with2(),
+                            Text(
+                              '${file.totalViews}',
+                              style: appTextStyle.medium14.copyWith(
+                                color: themeStore.appColors.textGrey2Color,
                               ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              //
-                              Icon(
-                                Icons.visibility_rounded,
-                                color: themeStore.appColors.iconGreyColor,
+                            ),
+                            HorizontalTextPadding.with8(),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: themeStore.appColors.textGrey2Color,
                               ),
-                              HorizontalTextPadding.with2(),
-                              Text(
-                                '${file.totalViews}',
-                                style: appTextStyle.semiBold16.copyWith(
-                                  color: themeStore.appColors.textGrey2Color,
-                                ),
+                              width: 3,
+                              height: 3,
+                            ),
+                            HorizontalTextPadding.with8(),
+                            Icon(
+                              Icons.file_download_sharp,
+                              color: themeStore.appColors.iconGreyColor,
+                              size: AppIconSize.size_20,
+                            ),
+                            HorizontalTextPadding.with2(),
+                            Text(
+                              '${file.totalDownloads}',
+                              style: appTextStyle.medium14.copyWith(
+                                color: themeStore.appColors.textGrey2Color,
                               ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                          ],
+                        ),
+                      ],
                     );
                   },
                 );
