f8ff8251d8296b468d632add1695bad5baa69ef0
nabil.alhafez <nabil.alhfz98@gmail.com>
2024-01-31 23:16:43 +0200

    Adds new safe-url pipe test

--- frontend/src/app/shared/pipes/safe-url.pipe.spec.ts
+++ frontend/src/app/shared/pipes/safe-url.pipe.spec.ts
@@ -1,5 +1,6 @@
 import { SafeUrlPipe } from "./safe-url.pipe";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { TestBed } from '@angular/core/testing';
 
 describe("SafeUrlPipe", () => {
   let pipe: SafeUrlPipe;
@@ -13,14 +14,15 @@ describe("SafeUrlPipe", () => {
   it("should create an instance of the pipe", () => {
     expect(pipe).toBeTruthy();
   });
-  
+
 
   // it("should call DomSanitizer's bypassSecurityTrustResourceUrl with the provided URL", () => {
   //   const url = "https://example.com";
   //   const expectedSafeUrl: SafeResourceUrl = {} as SafeResourceUrl; // Mock SafeResourceUrl
 
   //   // Mock DomSanitizer's bypassSecurityTrustResourceUrl method
-  //   sanitizer.bypassSecurityTrustResourceUrl.and.returnValue(expectedSafeUrl);
+  //   // spyOn(sanitizer, 'bypassSecurityTrustResourceUrl').and.returnValue(expectedSafeUrl);
+  //   // sanitizer.bypassSecurityTrustResourceUrl.and.returnValue(expectedSafeUrl);
 
   //   const result = pipe.transform(url);
 
@@ -54,7 +56,39 @@ describe("SafeUrlPipe", () => {
   //   expect(result).toBeNull();
   // });
 
-  // Add more test cases as needed to cover edge cases and other scenarios.
 });
 
- // ng test --include=src/app/shared/pipes/safe-url.pipe.spec.ts
+
+describe('SafeUrlPipe', () => {
+  let pipe: SafeUrlPipe;
+  let sanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+
+      providers: [SafeUrlPipe, DomSanitizer]
+    });
+
+    pipe = TestBed.inject(SafeUrlPipe);
+    sanitizer = TestBed.inject(DomSanitizer);
+  });
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  // it('should transform URL into SafeResourceUrl', () => {
+  //   const url = 'https://example.com';
+  //   // Spy on the bypassSecurityTrustResourceUrl method to return a mock SafeResourceUrl
+  //   spyOn(sanitizer, 'bypassSecurityTrustResourceUrl').and.returnValue('safeUrl');
+  //   const safeUrl = pipe.transform(url);
+  //   // Expect the sanitizer to have been called with the URL
+  //   expect(sanitizer.bypassSecurityTrustResourceUrl).toHaveBeenCalledWith(url);
+  //   // Expect the result to be a 'safeUrl' (or whatever your mock returns)
+  //   expect(safeUrl).toEqual('safeUrl');
+  // });
+
+
+});
+
+// ng test --include=src/app/shared/pipes/safe-url.pipe.spec.ts
