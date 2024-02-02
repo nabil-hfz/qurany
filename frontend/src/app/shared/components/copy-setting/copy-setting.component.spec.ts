ef66495bf9ff9a613c79d037a16fa3094cd9d36c
nabil.alhafez <nabil.alhfz98@gmail.com>
2024-02-02 23:59:57 +0200

    Edits copy setting test

--- frontend/src/app/shared/components/copy-setting/copy-setting.component.spec.ts
+++ frontend/src/app/shared/components/copy-setting/copy-setting.component.spec.ts
@@ -29,12 +29,12 @@ describe('CopySettingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
   it('should initialize settings with data provided', () => {
     expect(component.settings).toEqual([{ label: 'Test Label', key: 'test_key', checked: true }]);
   });
 
   it('should close the dialog with the correct list data on save', () => {
-    // const dialogRefSpy = spyOn(TestBed.get(MatDialogRef), 'close');
     component.list = [{ key: 'test_key', value: 'Test Value' }];
     component.save();
     expect(closeSpy).toHaveBeenCalledWith({ list: [{ key: 'test_key', value: 'Test Value' }] });
